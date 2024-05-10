6c171aac62c4e0d830765fb88efd07ce43b8a04a
interniums <intuition228@gmail.com>
2024-05-10 22:31:14 +0300

    doing header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import { useState } from "react"
+import Header from "./components/header-main/Header"
 
 function App() {
-  
+  const [isPreview, setIsPreview] = useState(false)
+
+	return (
+		<Header />
+	)
 }
 
 export default App
---
+++ src/components/header-main/Header.jsx
@@ -0,0 +1,20 @@
+import styles from '../../styles/Header.module.css'
+import githublogo from '../../assets/github-icon.png' 
+
+export default function Header() {
+
+	return (
+		<div className={styles.container}>
+			<div className={styles.credits}>
+				<a className={styles.anchor} href="https://github.com/interniums">
+					<p className={styles.creditsText}>Interniums</p>
+					<img 
+						src={githublogo} 
+						alt="github-logo" 
+						className={styles.logo}
+					/>
+				</a>
+			</div>
+		</div>
+	)
+}

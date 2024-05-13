4cf710ceb81a721d2267ffd3caf9da3fa47332d9
interniums <intuition228@gmail.com>
2024-05-13 22:40:09 +0300

    21

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,5 +1,6 @@
 import styles from '../../styles/Main.module.css'
 import cameraicon from '../../assets/cameraicon.svg'
+import TextField from '@mui/material/TextField'
 
 export default function Main() {
 
@@ -13,6 +14,49 @@ export default function Main() {
 				<div className={styles.photoContainer}>
 					<img className={styles.cameraicon} src={cameraicon} alt="" />
 					<p className={styles.phototext}>Add photo (optional)</p>
+				</div>
+				<div className={styles.nameContainer}>
+					<TextField
+						label="First name"
+						id="outlined-size-medium"
+						size="big"
+					/>
+					<TextField
+						label="Last name"
+						id="outlined-size-medium"
+						size="big"
+						fullWidth
+					/>
+				</div>
+			</section>
+			<section className={styles.section2}>
+				<div className={styles.mailContainer}>
+				<TextField
+						label="Email adress"
+						id="outlined-size-medium"
+						size="big"
+						fullWidth
+					/>
+					<TextField
+						label="Phone number"
+						id="outlined-size-medium"
+						size="big"
+						fullWidth
+					/>
+				</div>
+				<div className={styles.locationContainer}>
+					<TextField
+						label="Country"
+						id="outlined-size-medium"
+						size="big"
+						fullWidth
+					/>
+					<TextField
+						label="City"
+						id="outlined-size-medium"
+						size="big"
+						fullWidth
+					/>
 				</div>
 			</section>
 		</div>
